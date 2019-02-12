01acd0fb3ecc69265e8f889e2a0e23f33c20bb4c
alexVCrow <Flower230311>
2019-02-12 22:16:51 +0200

    4 commit

--- src/UserDto.java
+++ src/UserDto.java
@@ -4,4 +4,27 @@ public class UserDto {
     private String  name;
     private String age;
 
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
